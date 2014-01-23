2a491916ea6ac44f6b1488a128a4a5f61ac1d0c6
amandasaso <amanda@sumologic.com>
2014-01-23 12:30:53 -0800

    Reuploading Help topics in pages branch.

---
+++ Data/Alias.js
@@ -0,0 +1,136 @@
+MadCap.Utilities.Xhr._FilePathToXmlStringMap.Add(
+	'Alias',
+	'<?xml version=\"1.0\" encoding=\"utf-8\"?>' +
+	'<CatapultAliasFile>' +
+	'    <Map Name=\"search_ref\" Link=\"Search_Syntax.htm\" Skin=\"Default\" ResolvedId=\"1\" />' +
+	'    <Map Name=\"config_local\" Link=\"Configuring_a_Local_File_Source.htm\" Skin=\"Default\" ResolvedId=\"1000\" />' +
+	'    <Map Name=\"config_remote\" Link=\"Configuring_a_Remote_File_Source.htm\" Skin=\"Default\" ResolvedId=\"1001\" />' +
+	'    <Map Name=\"config_syslog\" Link=\"Configuring_a_Syslog_Source.htm\" Skin=\"Default\" ResolvedId=\"1002\" />' +
+	'    <Map Name=\"search\" Link=\"Search.htm\" Skin=\"Default\" ResolvedId=\"1003\" />' +
+	'    <Map Name=\"status\" Link=\"Status.htm\" Skin=\"Default\" ResolvedId=\"1004\" />' +
+	'    <Map Name=\"collectors\" Link=\"Collectors.htm\" Skin=\"Default\" ResolvedId=\"1005\" />' +
+	'    <Map Name=\"users\" Link=\"Users.htm\" Skin=\"Default\" ResolvedId=\"1006\" />' +
+	'    <Map Name=\"metadata\" Link=\"Adding_Metadata_to_Sources.htm\" Skin=\"Default\" ResolvedId=\"1007\" />' +
+	'    <Map Name=\"wildcards\" Link=\"Using_Wildcards_in_Paths.htm\" Skin=\"Default\" ResolvedId=\"1015\" />' +
+	'    <Map Name=\"config_winevents\" Link=\"Configuring_a_Windows_Event_Log_Source.htm\" Skin=\"Default\" ResolvedId=\"1011\" />' +
+	'    <Map Name=\"getting_started\" Link=\"Getting_Started.htm\" Skin=\"Default\" ResolvedId=\"1012\" />' +
+	'    <Map Name=\"test_connectivity\" Link=\"Testing_Connectivity.htm\" Skin=\"Default\" ResolvedId=\"1013\" />' +
+	'    <Map Name=\"winUNCsharepath\" Link=\"Collecting_Windows_Logs_from_a_UNC_Share_Path.htm\" Skin=\"Default\" ResolvedId=\"1009\" />' +
+	'    <Map Name=\"win_unc_path\" Link=\"Collecting_Windows_Logs_from_a_UNC_Share_Path.htm\" Skin=\"Default\" ResolvedId=\"1016\" />' +
+	'    <Map Name=\"faqsyslog\" Link=\"syslogfaq.htm\" Skin=\"Default\" ResolvedId=\"1020\" />' +
+	'    <Map Name=\"Account_Settings\" Link=\"Account_Settings.htm\" Skin=\"Default\" ResolvedId=\"2000\" />' +
+	'    <Map Name=\"Adding_a_New_Source\" Link=\"Adding_a_New_Source.htm\" Skin=\"Default\" ResolvedId=\"2001\" />' +
+	'    <Map Name=\"Editing_a_Collector_Name_or_Metadata\" Link=\"Editing_a_Collector_Name_or_Metadata.htm\" Skin=\"Default\" ResolvedId=\"2023\" />' +
+	'    <Map Name=\"Parse_Using__Parser_Name_\" Link=\"Parse_Using_Parser_Library.htm\" Skin=\"Default\" ResolvedId=\"2045\" />' +
+	'    <Map Name=\"Resetting_a_User_Password\" Link=\"Resetting_a_User_Password.htm\" Skin=\"Default\" ResolvedId=\"2056\" />' +
+	'    <Map Name=\"Ternary_Expressions\" Link=\"Ternary_Expressions.htm\" Skin=\"Default\" ResolvedId=\"2078\" />' +
+	'    <Map Name=\"Welcome\" Link=\"Welcome.htm\" Skin=\"Default\" ResolvedId=\"2089\" />' +
+	'    <Map Name=\"Adding_or_Editing_a_User\" Link=\"Adding_or_Editing_a_User.htm\" Skin=\"Default\" ResolvedId=\"2002\" />' +
+	'    <Map Name=\"Advanced_Installation_Topics\" Link=\"Advanced_Installation_Topics.htm\" Skin=\"Default\" ResolvedId=\"2003\" />' +
+	'    <Map Name=\"Casting_to_a_Number_Type\" Link=\"Casting_to_a_Number_Type.htm\" Skin=\"Default\" ResolvedId=\"2006\" />' +
+	'    <Map Name=\"Cisco_ASA\" Link=\"Cisco_ASA.htm\" Skin=\"Default\" ResolvedId=\"2007\" />' +
+	'    <Map Name=\"Command_Line_Installation_for_Linux\" Link=\"Command_Line_Installation_for_Linux.htm\" Skin=\"Default\" ResolvedId=\"2008\" />' +
+	'    <Map Name=\"Conditional_Expressions\" Link=\"Conditional_Expressions.htm\" Skin=\"Default\" ResolvedId=\"2010\" />' +
+	'    <Map Name=\"Deactivating_a_User\" Link=\"Deactivating_a_User.htm\" Skin=\"Default\" ResolvedId=\"2014\" />' +
+	'    <Map Name=\"Deciding_How_Many_Collectors\" Link=\"Deciding_How_Many_Collectors.htm\" Skin=\"Default\" ResolvedId=\"2015\" />' +
+	'    <Map Name=\"Deciding_Where_to_Install\" Link=\"Deciding_Where_to_Install.htm\" Skin=\"Default\" ResolvedId=\"2016\" />' +
+	'    <Map Name=\"Deleting_a_User\" Link=\"Deleting_a_User.htm\" Skin=\"Default\" ResolvedId=\"2017\" />' +
+	'    <Map Name=\"Deploying_a_Windows_Collector_Automatically\" Link=\"Deploying_a_Windows_Collector_Automatically.htm\" Skin=\"Default\" ResolvedId=\"2018\" />' +
+	'    <Map Name=\"Detecting_Patterns_with_Summarize\" Link=\"Detecting_Patterns_with_Summarize.htm\" Skin=\"Default\" ResolvedId=\"2021\" />' +
+	'    <Map Name=\"Exporting_Search_Results\" Link=\"Exporting_Search_Results.htm\" Skin=\"Default\" ResolvedId=\"2024\" />' +
+	'    <Map Name=\"Full_Text_Search\" Link=\"Full-Text_Search.htm\" Skin=\"Default\" ResolvedId=\"2027\" />' +
+	'    <Map Name=\"Getting_Started_Using_Search\" Link=\"Getting_Started_Using_Search.htm\" Skin=\"Default\" ResolvedId=\"2028\" />' +
+	'    <Map Name=\"Graphing_Search_Results\" Link=\"Graphing_Search_Results.htm\" Skin=\"Default\" ResolvedId=\"2029\" />' +
+	'    <Map Name=\"Group_Operator_and_Group_by_Functions\" Link=\"Group_Operator_and_Group-by_Functions.htm\" Skin=\"Default\" ResolvedId=\"2030\" />' +
+	'    <Map Name=\"Group_By_Functions\" Link=\"Group-By_Functions.htm\" Skin=\"Default\" ResolvedId=\"2031\" />' +
+	'    <Map Name=\"Influencing_the_Outcome\" Link=\"Influencing_the_Summarize_Outcome.htm\" Skin=\"Default\" ResolvedId=\"2032\" />' +
+	'    <Map Name=\"Intro_to_Sumo_Logic\" Link=\"Intro_to_Sumo_Logic.htm\" Skin=\"Default\" ResolvedId=\"2033\" />' +
+	'    <Map Name=\"Limit\" Link=\"Limit.htm\" Skin=\"Default\" ResolvedId=\"2035\" />' +
+	'    <Map Name=\"Linux_OS\" Link=\"Linux_OS.htm\" Skin=\"Default\" ResolvedId=\"2036\" />' +
+	'    <Map Name=\"Math_Expressions\" Link=\"Math_Expressions.htm\" Skin=\"Default\" ResolvedId=\"2038\" />' +
+	'    <Map Name=\"Metadata_Searches\" Link=\"Metadata_Searches.htm\" Skin=\"Default\" ResolvedId=\"2039\" />' +
+	'    <Map Name=\"Navigating_through_Messages\" Link=\"Navigating_through_Messages.htm\" Skin=\"Default\" ResolvedId=\"2040\" />' +
+	'    <Map Name=\"Parse__Anchor_\" Link=\"Parse__Anchor_.htm\" Skin=\"Default\" ResolvedId=\"2041\" />' +
+	'    <Map Name=\"Parse_Expressions\" Link=\"Parse_Expressions.htm\" Skin=\"Default\" ResolvedId=\"2042\" />' +
+	'    <Map Name=\"Parse_nodrop_Option\" Link=\"Parse_nodrop_Option.htm\" Skin=\"Default\" ResolvedId=\"2043\" />' +
+	'    <Map Name=\"Parse_Regex\" Link=\"Parse_Regex.htm\" Skin=\"Default\" ResolvedId=\"2044\" />' +
+	'    <Map Name=\"Parsing_and_Naming_a_Field\" Link=\"Parsing_and_Naming_a_Field.htm\" Skin=\"Default\" ResolvedId=\"2046\" />' +
+	'    <Map Name=\"Pausing_or_Canceling_a_Search\" Link=\"Pausing_or_Canceling_a_Search.htm\" Skin=\"Default\" ResolvedId=\"2047\" />' +
+	'    <Map Name=\"Pre_reqs_for_Windows\" Link=\"Pre-reqs-for-Windows.htm\" Skin=\"Default\" ResolvedId=\"2049\" />' +
+	'    <Map Name=\"Recommendations_for_Apache_Collections\" Link=\"Recommendations_for_Apache_Collections.htm\" Skin=\"Default\" ResolvedId=\"2051\" />' +
+	'    <Map Name=\"Recommendations_for_Collections\" Link=\"Recommendations_for_Collections.htm\" Skin=\"Default\" ResolvedId=\"2052\" />' +
+	'    <Map Name=\"Recommendations_for_Linux_Collections\" Link=\"Recommendations_for_Linux_Collections.htm\" Skin=\"Default\" ResolvedId=\"2053\" />' +
+	'    <Map Name=\"Recommendations_for_Tomcat_Collections\" Link=\"Recommendations_for_Tomcat_Collections.htm\" Skin=\"Default\" ResolvedId=\"2054\" />' +
+	'    <Map Name=\"Recommendations_for_Windows_IIS_Collections\" Link=\"Recommendations_for_Windows_IIS_Collections.htm\" Skin=\"Default\" ResolvedId=\"2055\" />' +
+	'    <Map Name=\"Running_a_Search_from_a_Notification_Email\" Link=\"Running_a_Search_from_an_Alert_Email.htm\" Skin=\"Default\" ResolvedId=\"2058\" />' +
+	'    <Map Name=\"Saving_a_Search\" Link=\"Saving_a_Search.htm\" Skin=\"Default\" ResolvedId=\"2059\" />' +
+	'    <Map Name=\"Scheduling_a_Search_with_Notification\" Link=\"Scheduling_a_Search.htm\" Skin=\"Default\" ResolvedId=\"2061\" />' +
+	'    <Map Name=\"Search_Reference_intro\" Link=\"Search_Reference_intro.htm\" Skin=\"Default\" ResolvedId=\"2065\" />' +
+	'    <Map Name=\"Searching_Surrounding_Messages\" Link=\"Searching_Surrounding_Messages.htm\" Skin=\"Default\" ResolvedId=\"2066\" />' +
+	'    <Map Name=\"Sharing_Searches\" Link=\"Sharing_Searches.htm\" Skin=\"Default\" ResolvedId=\"2068\" />' +
+	'    <Map Name=\"Sort__Limit__and_Timeslice_Expressions\" Link=\"Sort__Limit__and_Timeslice_Expressions.htm\" Skin=\"Default\" ResolvedId=\"2069\" />' +
+	'    <Map Name=\"Sort\" Link=\"Sort.htm\" Skin=\"Default\" ResolvedId=\"2070\" />' +
+	'    <Map Name=\"Starting_or_Stopping_a_Collector\" Link=\"Starting_or_Stopping_a_Collector.htm\" Skin=\"Default\" ResolvedId=\"2071\" />' +
+	'    <Map Name=\"Suggested_Searches\" Link=\"Suggested_Searches.htm\" Skin=\"Default\" ResolvedId=\"2072\" />' +
+	'    <Map Name=\"Summarize_Delta_Option\" Link=\"Summarize_Delta_Option.htm\" Skin=\"Default\" ResolvedId=\"2073\" />' +
+	'    <Map Name=\"Summarize_Operator\" Link=\"Summarize_Operator.htm\" Skin=\"Default\" ResolvedId=\"2074\" />' +
+	'    <Map Name=\"Sumo_Logic_Overview\" Link=\"Sumo_Logic_Overview.htm\" Skin=\"Default\" ResolvedId=\"2076\" />' +
+	'    <Map Name=\"The_User_Interface\" Link=\"The_User_Interface.htm\" Skin=\"Default\" ResolvedId=\"2079\" />' +
+	'    <Map Name=\"Time_Expressions_Reference\" Link=\"Time_Expressions_Reference.htm\" Skin=\"Default\" ResolvedId=\"2080\" />' +
+	'    <Map Name=\"Timeslice_Operator\" Link=\"Timeslice_Operator.htm\" Skin=\"Default\" ResolvedId=\"2081\" />' +
+	'    <Map Name=\"Unattended_Installation_from_a_Script\" Link=\"Unattended_Installation.htm\" Skin=\"Default\" ResolvedId=\"2084\" />' +
+	'    <Map Name=\"Understanding_Sources\" Link=\"Understanding_Sources.htm\" Skin=\"Default\" ResolvedId=\"2085\" />' +
+	'    <Map Name=\"Using_a_third_party_client_to_handle_SSH_in_Windows\" Link=\"Using_a_third-party_client_to_handle_SSH_in_Windows.htm\" Skin=\"Default\" ResolvedId=\"2086\" />' +
+	'    <Map Name=\"Using_Apache_Parser\" Link=\"Applying_Apache_Parser.htm\" Skin=\"Default\" ResolvedId=\"2087\" />' +
+	'    <Map Name=\"Using_the_Web_Application\" Link=\"Using_the_Web_Application.htm\" Skin=\"Default\" ResolvedId=\"2088\" />' +
+	'    <Map Name=\"Where\" Link=\"Where.htm\" Skin=\"Default\" ResolvedId=\"2090\" />' +
+	'    <Map Name=\"Wildcards_in_Full_Text_Searches\" Link=\"Wildcards_in_Full_Text_Searches.htm\" Skin=\"Default\" ResolvedId=\"2091\" />' +
+	'    <Map Name=\"avg\" Link=\"avg.htm\" Skin=\"Default\" ResolvedId=\"3000\" />' +
+	'    <Map Name=\"count_and_count_distinct\" Link=\"count_and_count_distinct.htm\" Skin=\"Default\" ResolvedId=\"3001\" />' +
+	'    <Map Name=\"first_and_last\" Link=\"first_and_last.htm\" Skin=\"Default\" ResolvedId=\"3003\" />' +
+	'    <Map Name=\"min_and_max\" Link=\"min_and_max.htm\" Skin=\"Default\" ResolvedId=\"3004\" />' +
+	'    <Map Name=\"standard_deviation\" Link=\"standard_deviation.htm\" Skin=\"Default\" ResolvedId=\"3005\" />' +
+	'    <Map Name=\"sum\" Link=\"sum.htm\" Skin=\"Default\" ResolvedId=\"3006\" />' +
+	'    <Map Name=\"Changing_Your_Password\" Link=\"Changing_Your_Password.htm\" Skin=\"Default\" ResolvedId=\"3007\" />' +
+	'    <Map Name=\"syslog_tcp_udp\" Link=\"syslogfaq.htm\" ResolvedId=\"3008\" />' +
+	'    <Map Name=\"Using_the_Search_Libraries\" Link=\"Using_the_Search_Libraries.htm\" Skin=\"Default\" ResolvedId=\"3009\" />' +
+	'    <Map Name=\"Filter_source_data\" Link=\"Filtering_data_sent_from_a_Source.htm\" Skin=\"Default\" ResolvedId=\"4000\" />' +
+	'    <Map Name=\"Run_as_user\" Link=\"Run_As_Another_User.htm\" Skin=\"Default\" ResolvedId=\"4001\" />' +
+	'    <Map Name=\"Schedule_search\" Link=\"Scheduling_a_Search.htm\" Skin=\"Default\" ResolvedId=\"4002\" />' +
+	'    <Map Name=\"Sumo_Logic_Accounts\" Link=\"Sumo_Logic_accounts.htm\" Skin=\"Default\" ResolvedId=\"4004\" />' +
+	'    <Map Name=\"About_Dashboards\" Link=\"About_Dashboards.htm\" ResolvedId=\"4005\" />' +
+	'    <Map Name=\"Editing_Dashboards\" Link=\"Editing_Dashboards.htm\" ResolvedId=\"4006\" />' +
+	'    <Map Name=\"Saving_Dashboards\" Link=\"Saving_Dashboards.htm\" ResolvedId=\"4007\" />' +
+	'    <Map Name=\"Sharing_Dashboards\" Link=\"Sharing_Dashboards.htm\" ResolvedId=\"4008\" />' +
+	'    <Map Name=\"Adding_Monitors\" Link=\"Adding_Monitors.htm\" ResolvedId=\"4010\" />' +
+	'    <Map Name=\"CRON_Expressions\" Link=\"Cron_jobs_in_Scripted_Sources.htm\" ResolvedId=\"4011\" />' +
+	'    <Map Name=\"config_script\" Link=\"Configuring_a_Script_Source.htm\" ResolvedId=\"4012\" />' +
+	'    <Map Name=\"Troubleshoot_script\" Link=\"Troubleshooting_Script_Issues.htm\" ResolvedId=\"4013\" />' +
+	'    <Map Name=\"Timestamps\" Link=\"Understanding_Time_Stamps.htm\" ResolvedId=\"4014\" />' +
+	'    <Map Name=\"RBAC\" Link=\"Using_RBAC_with_Sumo_Logic.htm\" ResolvedId=\"4015\" />' +
+	'    <Map Name=\"SAML_UI\" Link=\"setting_up_SAML.htm\" ResolvedId=\"4016\" />' +
+	'    <Map Name=\"SAML_UI_2\" Link=\"Optional_SAML_Features.htm\" ResolvedId=\"4017\" />' +
+	'    <Map Name=\"Hidden_test1\" Link=\"Collecting_from_a_Script_Action.htm\" Skin=\"Sumo_HTML5_Light\" ResolvedId=\"4019\" />' +
+	'    <Map Name=\"sessionize\" Link=\"Sessionize_Operator.htm\" Skin=\"Sumo_HTML5_Light\" ResolvedId=\"4020\" />' +
+	'    <Map Name=\"accum\" Link=\"Accum_Operator.htm\" ResolvedId=\"4021\" />' +
+	'    <Map Name=\"fields\" Link=\"Fields_Operator.htm\" ResolvedId=\"4022\" />' +
+	'    <Map Name=\"diff\" Link=\"Diff_Operator.htm\" ResolvedId=\"4023\" />' +
+	'    <Map Name=\"smooth\" Link=\"Smooth_Operator.htm\" ResolvedId=\"4024\" />' +
+	'    <Map Name=\"total\" Link=\"Total_Operator.htm\" ResolvedId=\"4025\" />' +
+	'    <Map Name=\"pct\" Link=\"Percentile_Operator.htm\" ResolvedId=\"4026\" />' +
+	'    <Map Name=\"transpose\" Link=\"Transpose_Operator.htm\" ResolvedId=\"4027\" />' +
+	'    <Map Name=\"kv\" Link=\"Keyvalue_operator.htm\" ResolvedId=\"4028\" />' +
+	'    <Map Name=\"trace\" Link=\"Trace_Operator.htm\" ResolvedId=\"4029\" />' +
+	'    <Map Name=\"Config_S3\" Link=\"Configuring_an_Amazon_S3_Source.htm\" ResolvedId=\"4031\" />' +
+	'    <Map Name=\"Scan_int\" Link=\"Setting_the__S3_Source_Scan_Interval.htm\" ResolvedId=\"4032\" />' +
+	'    <Map Name=\"Grant_S3_access\" Link=\"Granting_Access_to_an_S3_bucket.htm\" ResolvedId=\"4033\" />' +
+	'    <Map Name=\"Diff_Collectors\" Link=\"Difference_between_Collectors.htm\" ResolvedId=\"4034\" />' +
+	'    <Map Name=\"Upload_to_HTTP\" Link=\"Uploading_Files_to_an_HTTP_Source.htm\" ResolvedId=\"4036\" />' +
+	'    <Map Name=\"Gen_URL\" Link=\"Generating_a_new_URL.htm\" ResolvedId=\"4037\" />' +
+	'    <Map Name=\"CIRD_operator\" Link=\"CIDR_Operator.htm\" ResolvedId=\"4038\" />' +
+	'    <Map Name=\"isNull\" Link=\"isNull_Operator.htm\" ResolvedId=\"4039\" />' +
+	'    <Map Name=\"Prefs\" Link=\"Preferences.htm\" ResolvedId=\"4040\" />' +
+	'    <Map Name=\"Security_settings\" Link=\"Managing_Security_Settings.htm\" ResolvedId=\"4041\" />' +
+	'    <Map Name=\"Data_Forwarding\" Link=\"Data_Forwarding.htm\" ResolvedId=\"4042\" />' +
+	'</CatapultAliasFile>'
+);
